bd3e95b5a9e981e1feb1cbd9b56e0a9a8cf2db25
kailanebonfaticamargo-dev <kailanebonfaticamargo@gmail.com>
2025-09-14 12:25:54 -0300

    Update geometria.py

--- geometria.py
+++ geometria.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 "print('Bem-vindo ao projeto de geometria!')" 
--e "\ndef area_quadrado(lado):\n    return lado * lado\n\nprint('µrea do quadrado 3x3:', area_quadrado(3))" 
--e "\nimport math\n\ndef area_circulo(raio):\n    return math.pi * raio * raio\n\nprint('µrea do c¡rculo raio 2:', area_circulo(2))" 
+-e "\ndef area_quadrado(lado):\n    return lado * lado\n\nprint('Âµrea do quadrado 3x3:', area_quadrado(3))" 
+-e "\nimport math\n\ndef area_circulo(raio):\n    return math.pi * raio * raio\n\nprint('Âµrea do cÂ¡rculo raio 2:', area_circulo(2))" 
